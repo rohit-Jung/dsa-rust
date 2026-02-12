8c27cccf5835ad563fd54e3e49243d2421fab51f
rohitJung <rokshh28@gmail.com>
2026-02-12 12:25:37 +0545

    greedy: fractional

--- src/knapsack/fractional.rs
+++ src/knapsack/fractional.rs
@@ -1,32 +1,27 @@
 pub fn fractional_knapsack(values: Vec<i32>, weights: Vec<i32>, sack_capacity: i32) {
-    let mut items: Vec<(i32, i32, f64)> = Vec::new();
-
-    let mut capacity = sack_capacity as f64;
     let mut profit = 0.00;
+    let mut cap = sack_capacity;
+    let mut per_values: Vec<(f64, i32, i32)> = Vec::new();
 
     for i in 0..values.len() {
-        let ratio = weights[i] as f64 * values[i] as f64;
-        items.push((values[i], weights[i], ratio));
+        let per_value = values[i] as f64 / weights[i] as f64;
+        per_values.push((per_value, values[i], weights[i]));
     }
 
-    // sort the per values  (bubble sort ?) rust uses stable sort ?
-    items.sort_by(|a, b| b.2.partial_cmp(&a.2).unwrap());
+    per_values.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
 
-    // start picking max
-    for &(val, weight, ratio) in &items {
-        if capacity == 0.0 {
+    for &(ratio, val, weight) in &per_values {
+        if cap == 0 {
             break;
         }
-
-        if capacity >= weight as f64 {
-            capacity -= weight as f64;
+        if cap >= weight {
             profit += val as f64;
+            cap -= weight;
         } else {
-            profit += ratio * capacity;
-            // set capaity to 0
-            capacity = 0.00;
+            profit += ratio * cap as f64;
+            break;
         }
     }
 
-    println!("Maximum profit {:.2}", profit);
+    println!("Max profit is {profit}");
 }
